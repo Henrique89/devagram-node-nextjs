0a5df0d23c879384d3755e2fc129281daa5f0ee6
hesalves <hree.alves@gmail.com>
2022-05-09 21:39:28 -0300

    conexao mongodb

--- pages/login.ts
+++ pages/login.ts
@@ -1,19 +1,23 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import {conectarMongoDB} from '../../devagram-node-nextjs/middlewares/conectaMongoDB';
+import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
 
-export default(
+const endpointlogin = (
     req : NextApiRequest,
-    res : NextApiResponse
+    res : NextApiResponse <RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
         if (login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-                res.status(200).json({msg : ' Usuário autenticado com sucesso'});
+                return res.status(200).json({msg : ' Usuário autenticado com sucesso'});
 
             }
-            return res.status(405).json ({erro: 'Usuário ou senha não encontrado'});
+            return res.status(400).json ({erro: 'Usuário ou senha não encontrado'});
 
     }
     return res.status(405).json({erro : 'Método informado não é válido'});
 }
+
+export default conectarMongoDB(endpointlogin);
